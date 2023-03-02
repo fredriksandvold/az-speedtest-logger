d65fa532e35d682b5e31162f183214b2704565b9
Fredrik Sandvold <111272438+fres00@users.noreply.github.com>
2023-03-02 14:29:56 +0100

    My first SpeedTestLogger

---
+++ SpeedTestLogger/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using SpeedTest;
+using SpeedTest.Models;
+using System.Linq;
+using SpeedTestLogger;
+using SpeedTestLogger.Models;
+
+Console.WriteLine("Hello SpeedTestLogger!");
+
+var config = new LoggerConfiguration();
+var runner = new SpeedTestRunner(config.LoggerLocation);
+var testData = runner.RunSpeedTest();
+var results = new TestResult(
+    SessionId: Guid.NewGuid(),
+    User: config.UserId,
+    Device: config.LoggerId,
+    Timestamp: DateTimeOffset.Now.ToUnixTimeMilliseconds(),
+    Data: testData);
